c7b83164b1edde1ddfdc1d6505950abe416bf6c3
Awripedes <victororfeumerlo@gmail.com>
2022-07-19 19:45:35 -0300

    Teste realizado com sucesso. RPi é capaz de controlar 2 módulos L298N com 4 sinais PWM em hardware. Cada dupla de sinais irá para um módulo e cada dupla será espelhada em seu módulo para controlar ambos motores com os mesmos sinais.

--- Código/Teste-PWM/Teste_PWM.c
+++ Código/Teste-PWM/Teste_PWM.c
@@ -5,11 +5,17 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-#define PINO_PWM 18
+#define PINO_PWM11 12
+#define PINO_PWM12 13
+#define PINO_PWM21 18
+#define PINO_PWM22 19
 
 void SIGINT_Handler(){
     printf("%d: Encerrando\n", getpid());
-
+    gpioHardwarePWM(PINO_PWM11, 0, 0);
+    gpioHardwarePWM(PINO_PWM12, 0, 0);
+    gpioHardwarePWM(PINO_PWM21, 0, 0);
+    gpioHardwarePWM(PINO_PWM22, 0, 0);
     gpioTerminate();
     exit(1);
 }
@@ -22,13 +28,25 @@ int main(int argc, char** argv){
 
     signal(SIGINT, SIGINT_Handler);
 
-    gpioSetMode(PINO_PWM, PI_OUTPUT);
+    gpioSetMode(PINO_PWM11, PI_OUTPUT);
+    gpioSetMode(PINO_PWM12, PI_OUTPUT);
+    gpioSetMode(PINO_PWM21, PI_OUTPUT);
+    gpioSetMode(PINO_PWM22, PI_OUTPUT);
 
-    int frequencia = 1;
+    int frequencia = 10;
     int dutyCycle = 50;
 
     while(1){
-        gpioHardwarePWM(PINO_PWM, frequencia, dutyCycle*10000);
+        gpioHardwarePWM(PINO_PWM11, frequencia, dutyCycle*10000);
+        gpioHardwarePWM(PINO_PWM12, 0, 0);
+        gpioHardwarePWM(PINO_PWM21, frequencia, dutyCycle*10000);
+        gpioHardwarePWM(PINO_PWM22, 0, 0);
+        sleep(1);
+        gpioHardwarePWM(PINO_PWM11, 0, 0);
+        gpioHardwarePWM(PINO_PWM12, frequencia, dutyCycle*10000);
+        gpioHardwarePWM(PINO_PWM21, 0, 0);
+        gpioHardwarePWM(PINO_PWM22, frequencia, dutyCycle*10000);
+        sleep(1);
     }
 
     return 0;
