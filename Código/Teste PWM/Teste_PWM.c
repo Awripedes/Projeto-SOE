ef5a0c4ff726505259f9c031c21b22682a3f0595
Awripedes <victororfeumerlo@gmail.com>
2022-07-19 18:46:53 -0300

    Adicionando arquivos de teste do PWM

---
+++ Código/Teste PWM/Teste_PWM.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pigpio.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define PINO_PWM 18
+
+void SIGINT_Handler(){
+    printf("%d: Encerrando\n", getpid());
+
+    gpioTerminate();
+    exit(1);
+}
+
+int main(int argc, char** argv){
+    if( gpioInitialise() < 0 ){
+        printf("%d: Erro ao inicializar pigpio\n", getpid());
+        exit(-1);
+    }
+
+    signal(SIGINT, SIGINT_Handler);
+
+    gpioSetMode(PINO_PWM, PI_OUTPUT);
+    gpioSetPullUpDown(PINO_BOT, PI_PUD_UP);
+    gpioSetMode(PINO_BOT, PI_INPUT);
+
+    int frequencia = 1;
+    int dutyCycle = 50;
+
+    while(1){
+        gpioHardwarePWM(PINO_PWM, frequencia, dutyCycle*10000);
+    }
+
+    return 0;
+}
