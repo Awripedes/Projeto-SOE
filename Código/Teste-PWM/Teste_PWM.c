27ec2e923fff0a2f3cae23749c825892239ff846
Awripedes <victororfeumerlo@gmail.com>
2022-07-24 18:11:38 -0300

    Frequência do PWM adequada para motores DC

--- Código/Teste-PWM/Teste_PWM.c
+++ Código/Teste-PWM/Teste_PWM.c
@@ -5,11 +5,43 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-#define PINO_PWM 18
+#define PINO_PWM11 12
+#define PINO_PWM12 18
+#define PINO_PWM21 13
+#define PINO_PWM22 19
+
+#define ESQUERDA 0
+#define DIREITA 1
+#define PRA_FRENTE 1
+#define PRA_TRAS -1
+
+void sendSpeed(int lado, int sentido, int velocidade){
+    if(lado==ESQUERDA){
+        if(sentido==PRA_FRENTE){
+            gpioPWM(PINO_PWM11, velocidade);
+            gpioPWM(PINO_PWM12, 0);
+        }
+        if(sentido==PRA_TRAS){
+            gpioPWM(PINO_PWM11, 0);
+            gpioPWM(PINO_PWM12, velocidade);
+        }
+    }
+    if(lado==DIREITA){
+        if(sentido==PRA_FRENTE){
+            gpioPWM(PINO_PWM21, velocidade);
+            gpioPWM(PINO_PWM22, 0);
+        }
+        if(sentido==PRA_TRAS){
+            gpioPWM(PINO_PWM21, 0);
+            gpioPWM(PINO_PWM22, velocidade);
+        }
+    }
+}
 
 void SIGINT_Handler(){
     printf("%d: Encerrando\n", getpid());
-
+    sendSpeed(ESQUERDA, PRA_FRENTE, 0);
+    sendSpeed(DIREITA, PRA_TRAS, 0);
     gpioTerminate();
     exit(1);
 }
@@ -22,13 +54,33 @@ int main(int argc, char** argv){
 
     signal(SIGINT, SIGINT_Handler);
 
-    gpioSetMode(PINO_PWM, PI_OUTPUT);
+    int frequencia = 2000;
+    int direcao = 1;
 
-    int frequencia = 1;
-    int dutyCycle = 50;
+    gpioSetMode(PINO_PWM11, PI_OUTPUT);
+    gpioSetMode(PINO_PWM12, PI_OUTPUT);
+    gpioSetMode(PINO_PWM21, PI_OUTPUT);
+    gpioSetMode(PINO_PWM22, PI_OUTPUT);
+    gpioSetPWMfrequency(PINO_PWM11, frequencia);
+    gpioSetPWMfrequency(PINO_PWM12, frequencia);
+    gpioSetPWMfrequency(PINO_PWM21, frequencia);
+    gpioSetPWMfrequency(PINO_PWM22, frequencia);
 
     while(1){
-        gpioHardwarePWM(PINO_PWM, frequencia, dutyCycle*10000);
+        for(int i=100; i<255; i++){
+            printf("Velocidade atual: %d\n", i);
+            sendSpeed(0, direcao, i);
+            sendSpeed(1, direcao, i);
+            usleep(10000);
+        }
+        for(int i=255; i>35; i--){
+            printf("Velocidade atual: %d\n", i);
+            sendSpeed(0, direcao, i);
+            sendSpeed(1, direcao, i);
+            usleep(10000);
+        }
+        sendSpeed(0, 0, 0);
+        direcao = -direcao;
     }
 
     return 0;
